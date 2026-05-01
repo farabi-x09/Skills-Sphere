182e7aefad0c302abd57f6fa59ac2f8d828f5682
Farabi Ahmed <farabiahmed2005@gmail.com>
2026-05-01 11:15:21 +0600

    some card added in Popular courses

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -30,20 +30,12 @@ const TrendingCard = ({course}) => {
                 </h2>
             </div>
 
-            <div className='flex items-center gap-2 mt-2 text-gray-400 text-xs font-medium'>
+            <div className='flex items-center gap-2  text-gray-400 text-xs font-medium'>
                 <BsClock className='text-xs' />
                 <span>{duration}</span>
             </div>
         </div>
-        //  <div className='w-50   border-2 border-gray-300 rounded-lg relative'>
-        //            <Image className='w-full object-cover h-50 rounded-t-lg' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  src={image} alt={title} width={300} height={200} />
-        //            <div className='w-11/12 mx-auto'>
-        //            <h1 className='font-bold text-lg mt-4'>{title}</h1>
-                  
-        //             <p className='absolute top-0 right-0 mt-4 mr-4 bg-white p-2 rounded-xl flex  items-center gap-2'><BsStarFill className='text-amber-400'></BsStarFill> {rating}</p>
-        //            <button className='btn bg-blue-700 text-white hover:bg-blue-900 w-full my-5 py-2 rounded-full '>View Details</button>
-        //              </div>
-        //      </div>
+      
     );
 };
 
